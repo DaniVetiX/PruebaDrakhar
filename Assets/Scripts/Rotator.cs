467d8956af0a8225f0a168f753733b37edcffaa8
MeSientoYMeCallo99 <108568535+DaniVetiX@users.noreply.github.com>
2023-02-02 13:52:15 +0100

    Initial Commit

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    //Editor Variables
+    public bool isRotating;
+    public float Speed;
+    
+    //Local Variables
+    private float sum;
+
+    private void Update()
+    {
+        if (isRotating)
+        {
+            sum++;
+            this.transform.rotation = Quaternion.AngleAxis(30 * (sum * Speed), Vector3.up);
+        }
+    }
+}
